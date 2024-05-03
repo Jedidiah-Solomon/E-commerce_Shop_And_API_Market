36c32f26cfda6263a8fecb3e8467385bda52e825
Jedidiah Solomon Onwubiko <121108148+Jedidiah-Solomon@users.noreply.github.com>
2024-05-03 01:21:18 +0100

    New

--- src/scripts/contact.js
+++ src/scripts/contact.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('mc-embedded-subscribe-form');
     const successMessage = document.getElementById('success-message');
+    const errorMessage = document.getElementById('error-message');
   
     form.addEventListener('submit', function(e) {
       e.preventDefault();
@@ -15,20 +16,23 @@ document.addEventListener('DOMContentLoaded', function() {
       })
         .then((response) => {
           if (response.ok) {
+            errorMessage.textContent = '';
             successMessage.textContent = 'Thank you for your message! I will get back to you.';
             setTimeout(() => {
               window.location.href = '../../index.html';
-            }, 2000);
+            }, 5000);
   
             
             form.reset();
           } else {
-            successMessage.textContent = 'Oops! Something went wrong. Please try again.';
+            successMessage.textContent = '';
+            errorMessage.textContent = 'Oops! Something went wrong. Please try again.';
           }
         })
         .catch((error) => {
           console.error('Error submitting form:', error);
-          successMessage.textContent = 'Oops! Something went wrong. Please try again.';
+          successMessage.textContent = '';
+          errorMessage.textContent = 'Oops! Something went wrong. Please try again.';
         });
     });
   });
