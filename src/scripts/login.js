4a8bbdad3113141c8746fec33e73a5e993aa9512
Jedidiah Solomon Onwubiko <121108148+Jedidiah-Solomon@users.noreply.github.com>
2024-05-20 10:35:04 +0100

    Updated front pages

--- src/scripts/login.js
+++ src/scripts/login.js
@@ -1,3 +1,13 @@
+//Mobile  hamburgar menu
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.getElementById("hamburger");
+  const navbar = document.getElementById("navbar");
+
+  hamburger.addEventListener("click", function () {
+    navbar.classList.toggle("show");
+  });
+});
+
 document.addEventListener("DOMContentLoaded", function () {
   const loginForm = document.querySelector("form");
 
--- src/scripts/pre-custom.js
+++ src/scripts/pre-custom.js
@@ -1,3 +1,13 @@
+//Mobile  hamburgar menu
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.getElementById("hamburger");
+  const navbar = document.getElementById("navbar");
+
+  hamburger.addEventListener("click", function () {
+    navbar.classList.toggle("show");
+  });
+});
+
 document.addEventListener("DOMContentLoaded", () => {
   const slides = document.querySelectorAll(".hero .slide");
   const leftArrow = document.querySelector(".left-arrow");
